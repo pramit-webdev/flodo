3ef10e017b197a4adba6ad2e0fee319f3f86b907
Pramit Das <daspramit92@gmail.com>
2026-03-25 00:19:38 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -134,4 +134,5 @@ async def delete_task(task_id: UUID):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
